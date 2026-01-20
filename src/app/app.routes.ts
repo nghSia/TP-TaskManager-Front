c221c2501f7b7c1a16e79dd64a213cec88be8ea6
James Johnson <johnsonjw48@gmail.com>
2026-01-20 14:05:28 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Register} from './features/auth/register/register';
+import {Login} from './features/auth/login/login';
+import {TaskList} from './features/task/task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'register', component: Register},
+  {path: 'login', component: Login},
+  {path: 'tasks', component: TaskList}
+];
